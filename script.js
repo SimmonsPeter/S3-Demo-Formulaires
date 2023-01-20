f1626f4f408d1a6d4698f0280d0a2c98f83213e6
Maryse M <Maryse.Mongeau@cegepoutaouais.qc.ca>
2023-01-20 14:56:10 -0500

    Valdiation javascript et plus d'éléments de formulaire

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function valider(){
+function valider() {
+    let mdp1 = document.forms[0]["mdp1"].value;
+    let mdp2 = document.forms[0]["mdp2"].value;
 
-    alert("attention!");
-    return false; //Empêcher l'envoie du formulaire
+    if (mdp1 === mdp2 && mdp1.length >= 8) {
+        // On veut aussi avoir minimum 8 caractères
+        // On veut aussi avoir des chiffres et des lettres
+        return true
+    } else {
+        alert("Les 2 mots de passe doivent être identiques et contenir au moins 8 caractères")
+        return false; //Empêcher l'envoie du formulaire
+    }
 }
